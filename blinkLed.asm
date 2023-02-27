510143aa8dfe06ce141167e2c45776b355b5f321
Andrew-Negrut <45469110+Andrew-Negrut@users.noreply.github.com>
2023-02-27 11:26:22 -0600

    working

--- blinkLed.asm
+++ blinkLed.asm
@@ -4,19 +4,34 @@ main:
 ldi r24, 0b00000010
 sts 0x24, r24 ; set the DDRB
 sts 0x25, r24 ; set the value on digital pin 9
-call repeat
+rjmp repeat
 
 repeat:
+call predelay
 call delay
 ldi r24, 0b00000000
 sts 0x25, r24
+call predelay
 call delay
-ldi r24, 0b00000011
+ldi r24, 0b00000010
 sts 0x25, r24
 rjmp repeat
 
+predelay:
+ldi r16, 100
+ldi r17, 100
+ldi r18, 100
+ret
+
+; delay method created with help of chatGPT
 delay:
-ldi r24, 0b00000000
-ldi r24, 0b11111111
-ldi r24, 0b00000000
-ldi r24, 0b11111111
+dec r16
+brne delay
+dec r17 
+brne delay
+dec r18
+brne delay
+rjmp delayassist
+
+delayassist:
+ret
